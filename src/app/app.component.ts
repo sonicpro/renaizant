19cc18c249506fda11b8e468c178d2324aa1a3c9
Vladimir Tsurko <volodymyr.tsurko@valor-software.com>
2019-12-27 12:47:26 +0200

    Initial commit.

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ren-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Renaizant';
+}
---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ren-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss']
+})
+export class LandingPageComponent implements OnInit {
+  title: string;
+  text: string;
+  inputValue: string;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.title = 'Welcome!';
+    this.text = `Please tell us about your organization.<br />
+      It will help onboard employees at Renaizant`;
+    this.inputValue = 'START';
+  }
+
+}
